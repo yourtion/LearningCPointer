e9d7cd23520f35d1ce73169f639b3aa277bdf39f
YourtionGuo <yourtion@gmail.com>
2017-04-11 18:26:27 +0800

    code 2-3

---
+++ Chapter02/auto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void func(int a, int b) {
+  
+  int c, d;
+  
+  printf("func:&a..%p &b..%p\n", &a, &b);
+  printf("func:&c..%p &d..%p\n", &c, &d);
+}
+
+
+int main(int argc, char *argv[]) {
+  
+  int a, b;
+  
+  printf("main:&a..%p &b..%p\n", &a, &b);
+  func(1, 2);
+  
+  return 0;
+}
