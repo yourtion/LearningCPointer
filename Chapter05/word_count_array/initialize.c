25e238abc9fddeb4f92d22467ecb566ef9a8a305
YourtionGuo <yourtion@gmail.com>
2017-04-14 10:54:05 +0800

    code 5-5

---
+++ Chapter05/word_count_array/initialize.c
@@ -0,0 +1,13 @@
+#include "word_manage_p.h"
+
+Word    word_array[WORD_NUM_MAX];
+int     num_of_word;
+
+/************************************************************
+ * 初始化管理单词部分
+************************************************************/
+
+void word_initialize(void)
+{
+  num_of_word = 0;
+}
